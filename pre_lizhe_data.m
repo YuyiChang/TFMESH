e30e76dadfd82210f055e6e1a49846a3725d0eed
Yuyi Chang <22040659+YuyiChang@users.noreply.github.com>
2022-04-06 16:09:19 -0500

    add lizhe data preproc

---
+++ pre_lizhe_data.m
@@ -0,0 +1,28 @@
+clc;clear
+
+
+in_data_version = 'v7_6_combined';
+in_dir = ['data' filesep in_data_version filesep];
+
+out_dir = ['data' filesep in_data_version '_preproc' filesep];
+mkdir(out_dir)
+
+file_list = dir([in_dir '*.mat']);
+
+for i = 1:length(file_list)
+% i = 1
+    file_dir = [file_list(i).folder filesep file_list(i).name];
+    
+    load(file_dir)
+    
+    data_rear(:,6) = data_rear(:,6) + data_rear(:,9);
+    data_rear(:, 22) = 0;
+    
+    data_front(:, 22) = 1;
+    
+    data = sortrows([data_rear; data_front], [1, 2]);
+    
+    save([out_dir file_list(i).name], 'data')
+end
+
+%%
